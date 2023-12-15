0bea9e8244d76cf4aefb59bb28fecad308582103
rohit-gore <679.nine@gmail.com>
2023-12-15 09:28:49 +0530

    loops update

--- third.js
+++ third.js
@@ -1,1 +1,11 @@
 /* LOOPS and STRINGS IN JAVASCRIPT */
+
+for(let i=0; i<5; i++){
+    console.log("for loop");
+}
+
+let sum = 0;
+for(let i=1; i<=5; i++){
+    sum += i;
+}
+console.log("sum: " + sum);
