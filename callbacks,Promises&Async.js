454f232536d7cd7cfe284de69605732edd9505fa
rohit-gore <679.nine@gmail.com>
2024-03-10 22:14:23 +0530

    commit

---
+++ callbacks,Promises&Async.js
@@ -0,0 +1,6 @@
+// async await >> promise chains >> callback hell
+
+// Sync in JS --> jis sequence me code likha hai usi sequence me run hoga.
+console.log("one");
+console.log("two");
+console.log("three");
